d8a5a7389bde2915edf4149147a7a2f88e92e55a
zbirenbaum <zacharyobirenbaum@gmail.com>
2023-04-09 00:55:34 -0400

    add libs

---
+++ lib/tender/Tokens.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.10;
+
+interface IERC20 {
+  function name() external view returns (string memory);
+  function symbol() external view returns (string memory);
+  function decimals() external view returns (uint8);
+
+  function totalSupply() external view returns (uint256);
+  function balanceOf(address account) external view returns (uint256);
+  function transfer(address to, uint256 amount) external returns (bool);
+  function allowance(address owner, address spender) external view returns (uint256);
+  function approve(address spender, uint256 amount) external returns (bool);
+  function transferFrom(address from, address to, uint256 amount) external returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 amount);
+  event Approval(address indexed owner, address indexed spender, uint256 amount);
+}
+
+interface IWETH is IERC20 {
+  function deposit() external payable;
+  function withdraw(uint256) external;
+}
+
+interface IERC165 {
+  function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+interface IERC721 {
+  event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+  event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+  event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+  function supportsInterface(bytes4 interfaceId) external view returns (bool);
+
+  function balanceOf(address owner) external view returns (uint256 balance);
+  function ownerOf(uint256 tokenId) external view returns (address owner);
+  function approve(address to, uint256 tokenId) external;
+  function setApprovalForAll(address operator, bool _approved) external;
+  function getApproved(uint256 tokenId) external view returns (address operator);
+  function isApprovedForAll(address owner, address operator) external view returns (bool);
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId,
+    bytes calldata data
+  ) external;
+
+  function safeTransferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+  ) external;
+
+  function transferFrom(
+    address from,
+    address to,
+    uint256 tokenId
+  ) external;
+}
+
+interface IERC721Receiver {
+  function onERC721Received(
+    address operator,
+    address from,
+    uint256 tokenId,
+    bytes calldata data
+  ) external returns (bytes4);
+}
---
+++ src/VaultHelper.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: BSD-3-Clause
+pragma solidity ^0.8.10;
+import {IERC20, IWETH} from 'lib/interface/Tokens.sol';
+import {ICToken} from 'lib/interface/CToken.sol';
+import {Addresses} from 'lib/helpers/Addresses.sol';
+import {SafeMath} from 'lib/openzeppelin-contracts/contracts/utils/math/SafeMath.sol';
+import {IComptroller} from 'lib/interface/Comptroller.sol';
+import {IGlpManager, IGmxVault, IRewardRouterV2} from 'lib/interface/GMX.sol';
+import {ITenderPriceOracle} from 'lib/interface/TenderPriceOracle.sol';
+
+contract CTokenHelper is Addresses {
+  using SafeMath for uint;
+
+  function getUnderlying(ICToken cToken) public view returns (IERC20) {
+    return (cToken == tETH) ? wETH : ICToken(cToken).underlying();
+  }
+
+  function getCollateralFactor(ICToken cToken, bool vip) public view returns (uint) {
+    (,uint collateralFactor,,uint collateralFactorVip,,,,) = unitroller.markets(address(cToken));
+    return vip ? collateralFactorVip : collateralFactor;
+  }
+
+  function getLiquidationThreshold(ICToken cToken, bool vip) public view returns (uint) {
+    (,,uint liqThreshold,uint liqThresholdVip,,,,) = unitroller.markets(address(cToken));
+    return vip ? liqThresholdVip : liqThreshold;
+  }
+
+  function getUnderlyingPrice(ICToken cToken) public view returns (uint256) {
+    ITenderPriceOracle oracle = unitroller.oracle();
+    return oracle.getUnderlyingPrice(ICToken(cToken));
+  }
+
+  function getLeverageMultiplier(uint collateralFactor) public pure returns (uint) {
+    uint totalValueThreshold = 1e18;
+    uint maxValue = 100;
+    uint totalValueDividend = totalValueThreshold.sub(collateralFactor).div(1e16);
+    return maxValue.div(totalValueDividend);
+  }
+}
+
+contract GlpHelper is Addresses {
+  using SafeMath for uint;
+
+  function usdgAmounts(address token) public view returns (uint256){
+    return glpVault.usdgAmounts(token); // weth 127657111,442541096364249993
+  }
+
+  function getAumInUsdg() public view returns (uint256){
+    return glpManager.getAumInUsdg(true); // 428851069,305319770736134981
+  }
+
+  function getVaultPercentage(address token) public view returns (uint256){
+    return usdgAmounts(token).mul(1e18).div(getAumInUsdg());
+  }
+
+  function getGlpPrice() public view returns (uint256){
+    ITenderPriceOracle oracle = unitroller.oracle();
+    return oracle.getGlpAum().mul(1e18).div(glpToken.totalSupply());
+  }
+}
+
+contract VaultHelper is GlpHelper, CTokenHelper {
+}
